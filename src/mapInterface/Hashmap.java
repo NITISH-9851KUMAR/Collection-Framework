2bed28088c4ac0e0f0f4c0e5a47c289441fc1242
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-21 14:37:44 +0530

    Hash Map

--- src/mapInterface/Hashmap.java
+++ src/mapInterface/Hashmap.java
@@ -1,4 +1,20 @@
 package mapInterface;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Hashmap {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map= new HashMap<>();
+        map.put(1, "Aakash Kumar");
+        map.put(2, "Divyanshu Roy");
+        map.put(3, "Ankush Raj");
+        map.put(4, "Nitish Kumar");
+        map.put(4, "Nitish Kumar"); // It will not be added duplicate entry
+        map.put(5, "Himanshu Kumar");
+
+        System.out.println(map);
+
+    }
 }
