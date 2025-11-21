e8f87d73ac96a4720a5617b0d940ddb070a0d685
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-21 14:36:17 +0530

    Reverse ArrayList

--- src/collectionInterface/list_interface/arrayList/ReverseArrayList.java
+++ src/collectionInterface/list_interface/arrayList/ReverseArrayList.java
@@ -1,4 +1,40 @@
 package collectionInterface.list_interface.arrayList;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class ReverseArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList= new ArrayList<>();
+        arrayList.add(0);
+        arrayList.add(10);
+        arrayList.add(3);
+        arrayList.add(5);
+        arrayList.add(22);
+        arrayList.add(10);
+        arrayList.add(90);
+
+        System.out.println(arrayList);
+
+        // Reverse of arrayList
+        int fIdx= 0;
+        int lIdx= arrayList.size()-1;
+
+        // 0 10 3 5 22 10
+        // 0  1   2  3  4    5
+
+        int temp= 0;
+
+        while(fIdx<=lIdx){
+            temp= arrayList.get(fIdx);
+            arrayList.set(fIdx, arrayList.get(lIdx));
+            arrayList.set(lIdx, temp);
+            fIdx++;
+            lIdx--;
+        }
+        System.out.println(arrayList);
+
+    }
+
 }
