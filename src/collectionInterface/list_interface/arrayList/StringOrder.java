6bed0b18f18bce251802def4d6a12756d6027768
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-24 10:53:18 +0530

    Sorting of Character Contains List

--- src/collectionInterface/list_interface/arrayList/StringOrder.java
+++ src/collectionInterface/list_interface/arrayList/StringOrder.java
@@ -1,19 +1,26 @@
 package collectionInterface.list_interface.arrayList;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class StringOrder {
     public static void main(String[] args) {
 
-        List<Character> list= new ArrayList<>();
-        list.add('N');
-        list.add('I');
-        list.add('T');
-        list.add('I');
-        list.add('S');
-        list.add('H');
+        int[] arr = {15, 5, 9, 20, 8};
 
+        int max = 0;
+        // show above arr into the ascending Order
+        // 5, 9, 8, 15, 20
+
+        List<Character> list= new ArrayList<>();
+        list.add('Z');
+        list.add('Y');
+        list.add('T');
+        list.add('F');
+        list.add('E');
+        list.add('B');
+
+        // Print List Value
         System.out.println(list);
 
         // Get the Ascii Value of All Character
@@ -24,15 +31,30 @@ public class StringOrder {
         }
 
         // Code for sorting asciiVal ArrayList
-        int temp= 0;
-        int i= 0, j= asciiVal.size();
-        int max= asciiVal.get(i);
-        for(int k= i+1; k<j; k++){
-            if(max>asciiVal.get(k)){
-                temp= max;
-
+        // Outer loop traverse form 0th index to last index of an array
+        Outer_Loop:
+        for (int i = 0; i < asciiVal.size(); i++) {
+            // Inner Loop will traverse from i+1 Index
+            Inner_Loop:
+            for (int j = i + 1; j < asciiVal.size(); j++) {
+                max = asciiVal.get(i);
+                if (max > asciiVal.get(j)) {
+                    int temp= asciiVal.get(i);
+                    asciiVal.set(i, asciiVal.get(j));
+                    asciiVal.set(j, temp);
+                }
             }
         }
+
+        // change Integer value into character and store into list
+        for (int i = 0; i < asciiVal.size(); i++) {
+            int val = asciiVal.get(i);
+            char ch = (char) val;
+            list.set(i, ch);
+        }
+
+        // Print the list
+        System.out.println(list);
 
     }
 }
