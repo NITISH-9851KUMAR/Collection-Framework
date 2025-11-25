c7a76fefea66cf8b497736c4ee346aab5bf61f2c
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-25 14:12:49 +0530

    Node Of LinkedList basically it consists value and next field

---
+++ src/collectionInterface/list_interface/Linked_List/Node.java
@@ -0,0 +1,13 @@
+package collectionInterface.list_interface.Linked_List;
+
+public class Node{
+    int val;
+    Node next; // Default value null
+
+    Node(){}
+
+    Node(int val){
+        this.val= val;
+    }
+
+}
