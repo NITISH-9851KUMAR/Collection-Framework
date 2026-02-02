c8fd499b2f7f3f36e943c1846c2e42455a2378c7
Nitish Kumar <nitish983577@gmail.com>
2026-02-02 15:34:52 +0530

    Vector Class

---
+++ src/collectionInterface/list_interface/Array_List/VectorDemo.java
@@ -0,0 +1,22 @@
+package collectionInterface.list_interface.Array_List;
+
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+
+        Vector v= new Vector();
+        v.add(10);
+        v.add(20);
+        v.add(30);
+        v.add(40);
+        v.add(50);
+
+        System.out.println(v);
+
+        System.out.println(v.elementAt(3));
+        System.out.println(v.remove(2));
+        System.out.println(v);
+
+    }
+}
